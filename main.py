32fb26742452c76f32ccb3d6d22ebd231fb05043
hao <zhao5638@gmail.com>
2024-01-04 22:24:27 +0800

    add multipage

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from page.sim_nav_page import nav
+st.set_page_config(
+     page_title='Nav',
+     layout="wide",
+     initial_sidebar_state="expanded",
+     page_icon="🧬",
+)
+
+
+
+
+page_names_to_funcs = {
+    "Navgation": nav,
+    # "Plotting Demo": plotting_demo,
+    # "Mapping Demo": mapping_demo,
+    # "DataFrame Demo": data_frame_demo
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
